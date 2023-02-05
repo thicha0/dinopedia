0fdf5ccfcacb148a1ade775461fed90383a1fab6
Thicha <tib.chatelain@gmail.com>
2023-02-05 14:01:39 +0100

    good setup

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# dinopedia
+# 🦖 Dinopedia
+
+Encyclopedia of Dinosaurs
+
+## TODO
+- Search autocomplete
+- Styling
+- Add ChatGPT API
+
+## Sources
+- Dinosaur list from https://dinosaur-facts-api.shultzlab.com
+- Loading T-rex made with https://www.piskelapp.com/
+
+- Background outline: https://www.pngmart.com/image/310431
+
+- Inspiration animation: https://www.youtube.com/watch?v=XdTD--VRWFk&ab_channel=PS-HD4KMOTIONGRAPHICS
+- Color palette https://www.schemecolor.com/jungle-jungle.php
---
+++ script.js
@@ -0,0 +1,54 @@
+import dinolist from './list.json' assert {type: 'json'}
+
+let selectedDino = null
+
+let lastdinos = []
+
+function getRandomDino() {
+    const index = Math.floor(Math.random() * dinolist.length)
+    const dino = dinolist[index]
+
+    if (lastdinos.includes(dino)) return getRandomDino()
+    return dino
+}
+
+function addToLastDinos(dino) {
+    lastdinos.push(dino)
+    if (lastdinos.length > 5) {
+        lastdinos.shift()
+    }
+}
+
+const btnSearch = document.querySelector('.btn-search')
+const btnRandom = document.querySelector('.btn-random')
+const dinoInfos = document.querySelector('.current-dino')
+
+btnRandom.addEventListener('click', selectRandomDino)
+
+async function selectRandomDino() {
+    selectedDino = getRandomDino()
+    showLoading()
+}
+
+const loading = document.querySelector('.loading')
+const loadingText = document.querySelector('.loading-text')
+let loadingTexts = ['Hunting dinosaurs...', 'Retrieving fossils...', 'Time travelling...']
+function showLoading() {
+    const randomIndex = Math.floor(Math.random() * loadingTexts.length)
+    loadingText.textContent = loadingTexts[randomIndex]
+
+    dinoInfos.style.opacity = 0
+    loading.style.opacity = 1
+    return setTimeout(() => {
+        loading.style.opacity = 0
+        showDino()
+    }, 2000)
+}
+
+function showDino() {
+    setTimeout(() => {
+        dinoInfos.style.opacity = 1
+        dinoInfos.querySelector('.name').innerHTML = selectedDino.Name
+        dinoInfos.querySelector('.description').innerHTML = selectedDino.Description
+    }, 300)
+}
